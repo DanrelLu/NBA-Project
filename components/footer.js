e0151833f36cdddcd57fd48f1e95dcc3d5000ee6
SoftOnigiri <89949768+SoftOnigiri@users.noreply.github.com>
2024-05-29 18:14:23 -0600

    remove text

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className="w-full bg-orange-800 text-white text-center p-4">
             <p className="text-sm">
-                © {new Date().getFullYear()} NBA dsaddPlayer App. All rights reserved.
+                © {new Date().getFullYear()} NBA Player App. All rights reserved.
             </p>
             <div className="flex justify-center space-x-4 mt-2">
                 <a className="hover:text-orange-300">Terms of Service</a>
